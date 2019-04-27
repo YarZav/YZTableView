125d83af16046e56130f9dff453016f0548c10b8
ZavyalovYO <ZavyalovYO@taxcom.ru>
2019-04-27 21:42:25 +0500

    Added example target

---
+++ YZTableView/YZTableViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YZTableViewExample
+//
+//  Created by Yaroslav Zavyalov on 27/04/2019.
+//  Copyright © 2019 Yaroslav Zavyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
