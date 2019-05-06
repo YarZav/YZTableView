612526f54b1c99f1b863b28207bc806454d7aeaa
Yaroslav <zavyalovyo@taxcom.ru>
2019-05-06 11:35:19 +0300

    Inheritance from UITableView, not a UIView

--- YZTableView/YZTableView/YZTableView.swift
+++ YZTableView/YZTableView/YZTableView.swift
@@ -15,55 +15,19 @@ public protocol YZTableViewProtocol: class {
     func isContentHeightEqualTableViewHeight() -> Bool
 }
 
-open class YZTableView: UIView {
+open class YZTableView: UITableView {
     
-    public var tableView = UITableView()
-    public weak var delegate: YZTableViewProtocol?
-    
+    public weak var viewDelegate: YZTableViewProtocol?
     private var isLoading: Bool = false
-    private var refreshControl: UIRefreshControl?
-    
-    public weak var tableDelegate: UITableViewDelegate? {
-        didSet {
-            self.tableView.delegate = self.tableDelegate
-        }
-    }
-    
-    public weak var tableDataSource: UITableViewDataSource? {
-        didSet {
-            self.tableView.dataSource = self.tableDataSource
-        }
-    }
-    
-    public var cells: [AnyClass]? {
-        didSet {
-            self.cells?.forEach {
-                if let casted = $0.self as? UITableViewCell.Type {
-                    let reuseIdentifier = String(describing: casted.self)
-                    self.tableView.register($0, forCellReuseIdentifier: reuseIdentifier)
-                }
-            }
-        }
-    }
-
-    public init(cells: [AnyClass]) {
-        self.cells = cells
-        super.init(frame: .zero)
-    }
-    
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.createUI()
-    }
-    
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.createUI()
-    }
 }
 
 // MARK: - Public methods
 extension YZTableView {
+    
+    public func reusableCell<T: UITableViewCell>(indexPath: IndexPath) -> T {
+        let reuseIdentifier = String(describing: T.self)
+        return self.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! T
+    }
     
     public func startLoading() {
         if self.isLoading { return }
@@ -76,23 +40,22 @@ extension YZTableView {
         
         self.createRefreshControl()
     }
+    
+    public func setCells(_ cells: [AnyClass]) {
+        cells.forEach {
+            if let casted = $0.self as? UITableViewCell.Type {
+                let reuseIdentifier = String(describing: casted.self)
+                self.register($0, forCellReuseIdentifier: reuseIdentifier)
+            }
+        }
+    }
 }
 
 // MARK: - Privates
 extension YZTableView {
     
-    private func createUI() {
-        self.addSubview(self.tableView)
-        
-        let attributes: [NSLayoutConstraint.Attribute] = [.top, .left, .right, .bottom]
-        self.tableView.translatesAutoresizingMaskIntoConstraints = false
-        attributes.forEach {
-            NSLayoutConstraint(item: self.tableView, attribute: $0, relatedBy: .equal, toItem: self, attribute: $0, multiplier: 1, constant: 0).isActive = true
-        }
-    }
-    
     private func createRefreshControl() {
-        self.tableView.refreshControl = self.delegate?.getRefreshControl()
+        self.refreshControl = self.viewDelegate?.getRefreshControl()
     }
     
     private func removeRefreshControl() {
--- YZTableView/YZTableViewExample/YZTestViewController.swift
+++ YZTableView/YZTableViewExample/YZTestViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import YZTableView
 
+// MARK: - Test view controller
 class YZTestViewController: UIViewController {
     
-    private var tableView: YZTableView?
+    private var tableView = YZTableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,26 +27,55 @@ extension YZTestViewController {
         self.view.backgroundColor = .white
         self.title = "Test"
         
-        self.tableView = YZTableView(cells: [UITableViewCell.self])
-        self.tableView?.delegate = self
+        self.tableView.setCells([UITableViewCell.self])
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.viewDelegate = self
+        
+        self.view.addSubview(self.tableView)
+        
+        let attributes: [NSLayoutConstraint.Attribute] = [.top, .left, .right, .bottom]
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        attributes.forEach {
+            NSLayoutConstraint(item: self.tableView, attribute: $0, relatedBy: .equal, toItem: self.view, attribute: $0, multiplier: 1, constant: 0).isActive = true
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension YZTestViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension YZTestViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.tableView.reusableCell(indexPath: indexPath)
+        cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        return cell
     }
 }
 
 // MARK: - YZTableViewProtocol
 extension YZTestViewController: YZTableViewProtocol {
-    
+
     func getLoaderView() -> UIView? {
         return nil
     }
-    
+
     func getRefreshControl() -> UIRefreshControl? {
         return nil
     }
-    
+
     func getNoContentView() -> UIView? {
         return nil
     }
-    
+
     func isContentHeightEqualTableViewHeight() -> Bool {
         return false
     }
